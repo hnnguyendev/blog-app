3b2927b68753b692ba3a062f56fb6ef9307321c3
huunghia.nguyen <nghianguyenhuu848@gmail.com>
2025-05-24 21:55:52 +0700

    Update default theme for blog page

--- src/app/pages/blog/component/topbar-widget/topbar-widget.component.ts
+++ src/app/pages/blog/component/topbar-widget/topbar-widget.component.ts
@@ -1,10 +1,40 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, computed, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { LayoutService } from '@Layout/service/layout.service';
+import { $t } from '@primeng/themes';
+import Aura from '@primeng/themes/aura';
+import Lara from '@primeng/themes/lara';
+import Nora from '@primeng/themes/nora';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import { StyleClassModule } from 'primeng/styleclass';
+
+const presets = {
+  Aura,
+  Lara,
+  Nora
+} as const;
+
+declare type KeyOfType<T> = keyof T extends infer U ? U : never;
+
+declare type SurfacesType = {
+  name?: string;
+  palette?: {
+    0?: string;
+    50?: string;
+    100?: string;
+    200?: string;
+    300?: string;
+    400?: string;
+    500?: string;
+    600?: string;
+    700?: string;
+    800?: string;
+    900?: string;
+    950?: string;
+  };
+};
 
 @Component({
   selector: 'app-topbar-widget',
@@ -12,9 +42,170 @@ import { StyleClassModule } from 'primeng/styleclass';
   templateUrl: './topbar-widget.component.html',
   styleUrl: './topbar-widget.component.scss'
 })
-export class TopbarWidgetComponent {
+export class TopbarWidgetComponent implements OnInit {
+  private platformId = inject(PLATFORM_ID);
   public readonly layoutService = inject(LayoutService);
   public readonly router = inject(Router);
+
+  public selectedPrimaryColor = computed(() => {
+    return this.layoutService.layoutConfig().primary;
+  });
+
+  public primaryColors = computed<SurfacesType[]>(() => {
+    const presetPalette = presets[this.layoutService.layoutConfig().preset as KeyOfType<typeof presets>].primitive;
+    const colors = ['emerald', 'green', 'lime', 'orange', 'amber', 'yellow', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose'];
+    const palettes: SurfacesType[] = [{ name: 'noir', palette: {} }];
+
+    colors.forEach((color) => {
+      palettes.push({
+        name: color,
+        palette: presetPalette?.[color as KeyOfType<typeof presetPalette>] as SurfacesType['palette']
+      });
+    });
+
+    return palettes;
+  });
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.onPresetChange(this.layoutService.layoutConfig().preset);
+    }
+  }
+
+  public getPresetExt() {
+    const color: SurfacesType = this.primaryColors().find((c) => c.name === this.selectedPrimaryColor()) || {};
+    const preset = this.layoutService.layoutConfig().preset;
+
+    if (color.name === 'noir') {
+      return {
+        semantic: {
+          primary: {
+            50: '{surface.50}',
+            100: '{surface.100}',
+            200: '{surface.200}',
+            300: '{surface.300}',
+            400: '{surface.400}',
+            500: '{surface.500}',
+            600: '{surface.600}',
+            700: '{surface.700}',
+            800: '{surface.800}',
+            900: '{surface.900}',
+            950: '{surface.950}'
+          },
+          colorScheme: {
+            light: {
+              primary: {
+                color: '{primary.950}',
+                contrastColor: '#ffffff',
+                hoverColor: '{primary.800}',
+                activeColor: '{primary.700}'
+              },
+              highlight: {
+                background: '{primary.950}',
+                focusBackground: '{primary.700}',
+                color: '#ffffff',
+                focusColor: '#ffffff'
+              }
+            },
+            dark: {
+              primary: {
+                color: '{primary.50}',
+                contrastColor: '{primary.950}',
+                hoverColor: '{primary.200}',
+                activeColor: '{primary.300}'
+              },
+              highlight: {
+                background: '{primary.50}',
+                focusBackground: '{primary.300}',
+                color: '{primary.950}',
+                focusColor: '{primary.950}'
+              }
+            }
+          }
+        }
+      };
+    } else {
+      if (preset === 'Nora') {
+        return {
+          semantic: {
+            primary: color.palette,
+            colorScheme: {
+              light: {
+                primary: {
+                  color: '{primary.600}',
+                  contrastColor: '#ffffff',
+                  hoverColor: '{primary.700}',
+                  activeColor: '{primary.800}'
+                },
+                highlight: {
+                  background: '{primary.600}',
+                  focusBackground: '{primary.700}',
+                  color: '#ffffff',
+                  focusColor: '#ffffff'
+                }
+              },
+              dark: {
+                primary: {
+                  color: '{primary.500}',
+                  contrastColor: '{surface.900}',
+                  hoverColor: '{primary.400}',
+                  activeColor: '{primary.300}'
+                },
+                highlight: {
+                  background: '{primary.500}',
+                  focusBackground: '{primary.400}',
+                  color: '{surface.900}',
+                  focusColor: '{surface.900}'
+                }
+              }
+            }
+          }
+        };
+      } else {
+        return {
+          semantic: {
+            primary: color.palette,
+            colorScheme: {
+              light: {
+                primary: {
+                  color: '{primary.500}',
+                  contrastColor: '#ffffff',
+                  hoverColor: '{primary.600}',
+                  activeColor: '{primary.700}'
+                },
+                highlight: {
+                  background: '{primary.50}',
+                  focusBackground: '{primary.100}',
+                  color: '{primary.700}',
+                  focusColor: '{primary.800}'
+                }
+              },
+              dark: {
+                primary: {
+                  color: '{primary.400}',
+                  contrastColor: '{surface.900}',
+                  hoverColor: '{primary.300}',
+                  activeColor: '{primary.200}'
+                },
+                highlight: {
+                  background: 'color-mix(in srgb, {primary.400}, transparent 84%)',
+                  focusBackground: 'color-mix(in srgb, {primary.400}, transparent 76%)',
+                  color: 'rgba(255,255,255,.87)',
+                  focusColor: 'rgba(255,255,255,.87)'
+                }
+              }
+            }
+          }
+        };
+      }
+    }
+  }
+
+  public onPresetChange(event: any) {
+    this.layoutService.layoutConfig.update((state) => ({ ...state, preset: event }));
+    const preset = presets[event as KeyOfType<typeof presets>];
+    $t().preset(preset).preset(this.getPresetExt()).use({ useDefaultOptions: true });
+  }
 
   public toggleDarkMode() {
     this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
